dc18badec8792a8f6c4e3f0df2f07d0cdc10034d
Abhijit Parlapalli <abhijit.parla01@gmail.com>
2024-01-18 20:06:10 +0530

    apostrophe removed

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -8,7 +8,7 @@ const Testimonials = () => {
         <h1 className='text-4xl font-semibold p-5 pt-10 max-w-3xl'>Loved by business worldwide.</h1>
       </div>
       <div className='self-center'>
-        <h2 className='text-center mb-5 text-lg text-slate-500 max-w-3xl'>Our software is so simple that people can't help but fall in love with it. Simplicity is easy when you just skip tons of mission-critical features.</h2>
+        <h2 className='text-center mb-5 text-lg text-slate-500 max-w-3xl'>Our software is so simple that people cant help but fall in love with it. Simplicity is easy when you just skip tons of mission-critical features.</h2>
       </div>
       <div className='flex flex-wrap justify-center max-w-4xl self-center'>
         <TestimonialCard/>
